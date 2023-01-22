50ccd847370e44cfb2f636cc35ea640eeefc15ea
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-01-22 19:15:14 -0300

    Update purpleQuestions.js

--- src/data/purpleQuestions.js
+++ src/data/purpleQuestions.js
@@ -4,4 +4,10 @@ export const Questions = [
     choices: ["o pagamento do 13º salário proporcional.", "o direito à liberação do fundo de garantia do tempo de serviço, sem o acréscimo de 40%.", "a indenização de férias vencidas acrescidas de 1/3, quando completado o período aquisitivo.", "o aviso prévio indenizado, no prazo mínimo."],
     correct: 3,
   },
+  {
+    question: "Quando surgiu a CLT?",
+    choices: ["1943", "1944", "1945", "1988"],
+    correct: 0,
+  },
+];
 ];
