7be3861ec8be9f6f0ab07de9620e90e8bc5fe7a6
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-01-22 17:38:11 -0300

    Update purpleQuestions.js

--- src/data/purpleQuestions.js
+++ src/data/purpleQuestions.js
@@ -1,7 +1,7 @@
 export const Questions = [
   {
-    question: "Lorem ipsum dolor sit amet",
-    choices: ["Lorem", "Lorem", "Lorem", "Lorem"],
-    correct: 0,
+    question: "De acordo com a CLT, na despedida por justa causa, o empregado tem assegurado:",
+    choices: ["o pagamento do 13º salário proporcional.", "o direito à liberação do fundo de garantia do tempo de serviço, sem o acréscimo de 40%.", "a indenização de férias vencidas acrescidas de 1/3, quando completado o período aquisitivo.", "o aviso prévio indenizado, no prazo mínimo."],
+    correct: 3,
   },
 ];
