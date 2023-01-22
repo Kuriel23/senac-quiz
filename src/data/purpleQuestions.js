f1f75fe392c40f6d2eca23122f43f1761048c1b0
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-01-22 20:25:56 -0300

    Update purpleQuestions.js

--- src/data/purpleQuestions.js
+++ src/data/purpleQuestions.js
@@ -14,4 +14,19 @@ export const Questions = [
     choices: ["Certo", "Errado"],
     correct: 0,
   },
+  {
+    question: "Conforme descrito na CLT, o adicional notruno é realizado no horário de trabalho:",
+    choices: ["das 23 horas às 06 da manhã", "conforme descrito em cada convenção ou acordo coletivo de trabalho de acordo com cada categoria", "das 00:00 horas até às 05:00 da manhã", "das 22 horas às 05 da manhã"],
+    correct: 3,
+  },
+  {
+    question: "Com relação ao trabalho noturno, perigoso ou insalubre, realizado por trabalhadores efetivos, a Constituição Federal dispôe que poderá ocorrer somente:",
+    choices: ["a partir de 14 anos", "a partir de 16 anos", "a partir de 18 anos", "quando o profissional for qualificado e estiver em perfeitas condições de saúde"],
+    correct: 2,
+  },
+   {
+    question: "NR7 - Ao assinalar 'inapto' em um Atestado de Saúde Ocupacional (ASO), o médico está afirmando:",
+    choices: ["que o empregado não pode ser demitido", "que o empregado não pode/poderia trabalhar em determinada função", "que a pessoa não pode ser contratada", "todas as alternativas anteriores estão corretas"],
+    correct: 1,
+  },
 ];
