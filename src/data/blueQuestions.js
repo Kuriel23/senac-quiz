39586d41d84287802115be2b0f80a6e8addb728f
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-01-22 20:35:12 -0300

    Update blueQuestions.js

--- src/data/blueQuestions.js
+++ src/data/blueQuestions.js
@@ -1,7 +1,32 @@
 export const Questions = [
   {
-    question: "Quando surgiu a CLT?",
-    choices: ["1943", "1944", "1945", "1988"],
+    question: "Assinale a alternativa com a situação em que o FGTS não poderá ser sacado:",
+    choices: ["Aposentadoria por invalidez.", "Suspensão do trabalho avulso por período igual ou superior a 60 (sessenta) dias.", "Rescisão do contrato por culpa recíproca ou força maior.", "Rescisão do contrato por falecimento do empregador individual.",],
+    correct: 1,
+  },
+  {
+    question: "O FGTS é um valor pago pela empresa e deve ser descontado do trabalhador.",
+    choices: ["Certo", "Errado"],
+    correct: 1,
+  },
+  {
+    question: "Em caso de falecimento do trabalhador, sua conta do FGTS poderá ser movimentada por dependente habilitados ou alvará judicial indicando os sucessores do trabalhador falecido.",
+    choices: ["Certo", "Errado"],
+    correct: 0,
+  },
+  {
+    question: "Qual o significado da sigla FGTS?",
+    choices: ["Fundação Garantidora do Total de Serviços", "Fundo de Garantia por Tempo de Seguridade", "Fundação Garantidora por Tempo de Serviço", "Fundo de Garantia por Tempo de Serviço"],
+    correct: 3,
+  },
+  {
+    question: "Têm direito ao FGTS os trabalhadores individuais ou autônomos, ou seja, pessoas que não possuem vínculo empregatício.",
+    choices: ["Certo", "Errado"],
+    correct: 1,
+  },
+   {
+    question: "O FGTS tem como objetivo auxiliar o trabalhador financeiramente, caso este seja demitido, observadas determinadas regras.",
+    choices: ["Certo", "Errado"],
     correct: 0,
   },
 ];
