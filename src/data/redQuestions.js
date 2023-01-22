c035c29beab25ae54acc997614470a135bdf1abc
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-01-22 19:56:23 -0300

    Update redQuestions.js

--- src/data/redQuestions.js
+++ src/data/redQuestions.js
@@ -1,7 +1,47 @@
 export const Questions = [
   {
-    question: "Lorem ipsum dolor sit amet",
-    choices: ["Lorem", "Lorem", "Lorem", "Lorem"],
+    question: "De onde é a invenção do chuveiro elétrico?",
+    choices: ["França", "Brasil", "Inglaterra", "Austrália"],
+    correct: 1,
+  },
+  {
+    question: "O que significa a sigla IST?",
+    choices: ["Infecções Sexualmente Transmissíveis", "Inspeção de Segurança Trabalhista", "Instauração da Seguridade Trabalhista", "Influência Social no Trabalho"],
     correct: 0,
+  },
+  {
+    question: "Como se chamam os vasos que transportam o sangue do coração para o corpo?",
+    choices: ["Veias", "Átrios", "Ventrículos", "Artérias"],
+    correct: 3,
+  },
+  {
+    question: "O que é PIX?",
+    choices: ["Meio de pagamento instantâneo criado pelo Banco Central", "Plano de Investimento", "Forma de Pagamento Presencial", "Conjunto de Formas de Pagamento"],
+    correct: 0,
+  },
+  {
+    question: "Qual das alternativas contém apenas vacinas contra a Covid-19?",
+    choices: ["VIP/VOP & Astrazeneca", "BioTech & CoronaVac", "CoronaVac & Astrazeneca", "RotaVirus & Sputnik"],
+    correct: 2,
+  },
+  {
+    question: "Geralmente, quantos ossos um ser humano sem anomalias possui?",
+    choices: ["126", "200", "206", "180"],
+    correct: 2,
+  },
+  {
+    question: "Qual a maior floresta tropical do mundo?",
+    choices: ["Pantanal", "Floresta Tropical", "Pampas", "Floresta Amazônica"],
+    correct: 3,
+  },
+  {
+    question: "Kryptonita é a fraqueza de qual super-herói?",
+    choices: ["Hulk", "Super Homem", "Homem Aranha", "Batman"],
+    correct: 1,
+  },
+  {
+    question: "Quem pintou o famoso quadro da Mona Lisa?",
+    choices: ["Salvador Dali", "Van Gogh", "Leonardo da Vinci", "Tarsila do Amaral"],
+    correct: 3,
   },
 ];
