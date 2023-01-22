d9ef90b207a72c1dbaf6de5cdb0939afa53b441c
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-01-22 19:19:17 -0300

    Update purpleQuestions.js

--- src/data/purpleQuestions.js
+++ src/data/purpleQuestions.js
@@ -2,7 +2,7 @@ export const Questions = [
   {
     question: "De acordo com a CLT, na despedida por justa causa, o empregado tem assegurado:",
     choices: ["o pagamento do 13º salário proporcional.", "o direito à liberação do fundo de garantia do tempo de serviço, sem o acréscimo de 40%.", "a indenização de férias vencidas acrescidas de 1/3, quando completado o período aquisitivo.", "o aviso prévio indenizado, no prazo mínimo."],
-    correct: 3,
+    correct: 2,
   },
   {
     question: "Quando surgiu a CLT?",
