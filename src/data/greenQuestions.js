7f72914710912e1ee9b8e42b9c00f951b9a76fa9
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-01-22 21:20:37 -0300

    Update greenQuestions.js

--- src/data/greenQuestions.js
+++ src/data/greenQuestions.js
@@ -1,6 +1,6 @@
 export const Questions = [
   {
-    question: "Uma renomada empresa de jóias lançou um novo anel que continha três pedras de diamante. Essa mesma empresa pretendia fabricar somente onze anéis da mesma coleção, utilizando o mesmo tipo de pedra. Qunatas pedras de diamante seriam necessárias para fabricar os onze anéis?",
+    question: "Uma renomada empresa de jóias lançou um novo anel que continha três pedras de diamante. Essa mesma empresa pretendia fabricar somente onze anéis da mesma coleção, utilizando o mesmo tipo de pedra. Quantas pedras de diamante seriam necessárias para fabricar os onze anéis?",
     choices: ["36 pedras de diamante", "39 pedras de diamante", "33 pedras de diamante", "30 pedras de diamante"],
     correct: 2,
   },
