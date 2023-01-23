bd62c88fdb70c50ec747cc4850a6b258d0c7c511
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-01-22 21:17:55 -0300

    Update greenQuestions.js

--- src/data/greenQuestions.js
+++ src/data/greenQuestions.js
@@ -1,7 +1,7 @@
 export const Questions = [
   {
-    question: "Lorem ipsum dolor sit amet",
-    choices: ["Lorem", "Lorem", "Lorem", "Lorem"],
-    correct: 0,
+    question: "Uma renomada empresa de jóias lançou um novo anel que continha três pedras de diamante. Essa mesma empresa pretendia fabricar somente onze anéis da mesma linha. Qunatas pedras de diamante seria necessárias para fabricar os onze anéis?",
+    choices: ["36 pedras de diamante", "39 pedras de diamante", "33 pedras de diamante", "30 pedras de diamante"],
+    correct: 2,
   },
 ];
