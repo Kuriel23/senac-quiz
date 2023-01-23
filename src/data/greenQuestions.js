9f49e60f3c4c85e45453737cd30ef4a9ccf76e4c
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-01-23 11:13:29 +0000

    Update greenQuestions.js

--- src/data/greenQuestions.js
+++ src/data/greenQuestions.js
@@ -6,17 +6,17 @@ export const Questions = [
   },
   {
     question: "Júllia foi à Farmácia Preço Baixo comprar algodão com R$ 9,00 que recebeu de sua mãe. O balconista informou que um pacote de algodão custa R$ 1,25. Se Pérola comprou 7 pacotes de algodão, quanto lhe sobrou de troco?",
-    choices: "R$1,25", "0,75", "0,45", "0,25",
+    choices: ["R$1,25", "0,75", "0,45", "0,25"],
     correct: 3,
   },
   {
     question: "Quanto é 2+2+6x2?",
-    choices: "10", "12", "14", "16",
+    choices: ["10", "12", "14", "16"],
     correct: 1,
   },
   {
     question: "Mário pretende realizar uma viagem utilizando um veículo cujo consumo médio de combustível é de 10 Km/L. Sabendo que percorrerá 300 000 m, é correto afirmar que o volume de combustível a ser consumido será de:",
-    choices: "15", "30", "300", "3000",
+    choices: ["15", "30", "300", "3000"],
     correct: 1,
   },  
  ];
