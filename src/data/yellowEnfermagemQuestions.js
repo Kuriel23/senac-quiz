b5048e97e639dd6654d0258496a3d8d824f57cf7
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-03-19 17:07:26 -0300

    Update yellowEnfermagemQuestions.js

--- src/data/yellowEnfermagemQuestions.js
+++ src/data/yellowEnfermagemQuestions.js
@@ -1,57 +1,73 @@
 export const Questions = [
   {
     question:
-      "De acordo com a CLT, na despedida por justa causa, o empregado tem assegurado:",
+      "Um dos cuidados que os profissionais de saúde têm que ter com doentes com diagnóstico de tuberculose pulmonar é a precaução respiratória. Sendo assim, é recomendado que os profissionais utilizem a máscara:",
     choices: [
-      "o pagamento do 13º salário proporcional.",
-      "o direito à liberação do fundo de garantia do tempo de serviço, sem o acréscimo de 40%.",
-      "a indenização de férias vencidas acrescidas de 1/3, quando completado o período aquisitivo.",
-      "o aviso prévio indenizado, no prazo mínimo.",
+      "N95",
+      "tipo cirúrgica",
+      "de Venturi",
+      "de algodão",
+    ],
+    correct: 0,
+  },
+  
+  {
+    question:
+      "Segundo a Agência Nacional de Vigilância Sanitária (ANVISA), as “substâncias ou preparações destinadas à higienização, desinfecção ou desinfestação domiciliar, em ambientes coletivos e/ou públicos, em lugares de uso comum e no tratamento da água”, denomina-se:",
+    choices: [
+      "Produtos Sanitários",
+      "Saneantes Domissanitários",
+      "Produtos de limpeza e higiênicos",
+      "Desinfetantes",
+    ],
+    correct: 1,
+  },
+    
+  {
+    question:
+      "Considera como risco de natureza físico-química a capacidade de o produto reagir com outra substância, produzindo fenômenos físicos como calor, combustão ou explosão ou, ainda, produzindo outra substância tóxica. São proibições:",
+    choices: [
+      "Manuseio do produto saneantes com o uso de Equipamentos de Proteção Individual (EPI) apropriados.",
+      "Uso de luvas resistentes ao produto, preferencialmente de punho longo.",
+      "Reaproveitamento de embalagens vazias com produtos saneantes diferentes do rótulo original.",
+      "Uso de avental impermeável.",
     ],
     correct: 2,
   },
-  {
-    question: "Quando surgiu a CLT?",
-    choices: ["1943", "1944", "1945", "1988"],
-    correct: 0,
-  },
-  {
+  
+    {
     question:
-      "A remuneração do trabalho noturno será feita na base da remuneração do trabalho diurno, acrescida de 25%.",
-    choices: ["Certo", "Errado"],
-    correct: 0,
-  },
-  {
-    question:
-      "Conforme descrito na CLT, o adicional noturno é realizado no horário de trabalho:",
+      "Considerando o risco de transmissão e infecção, em uma unidade de atendimento ambulatorial, recebe a classificação como área crítica a sala de:",
     choices: [
-      "das 23 horas às 06 da manhã",
-      "conforme descrito em cada convenção ou acordo coletivo de trabalho de acordo com cada categoria",
-      "das 00:00 horas até às 05:00 da manhã",
-      "das 22 horas às 05 da manhã",
+      "Inalação coletiva.",
+      "Inalação individual.",
+      "Curativos e suturas.",
+      "Reidratação oral e venosa.",
+    ],
+    correct: 2,
+  },
+  
+      {
+    question:
+      "A assepsia pode ser definida como o conjunto de medidas adotadas para:",
+    choices: [
+      "A destruição dos micro-organismos de superfícies.",
+      "A destruição dos micro-organismos potenciais para causar infecção.",
+      "Inibir o crescimento de micro-organismos ou removê-los da superfície da pele ou mucosas.",
+      "Impedir a penetração de micro-organismos num ambiente que não os tem.",
     ],
     correct: 3,
   },
-  {
+  
+        {
     question:
-      "Com relação ao trabalho noturno, perigoso ou insalubre, realizado por trabalhadores efetivos, a Constituição Federal dispõe que poderá ocorrer somente:",
+      "O processo utilizado para destruir todas as formas de vida microbiana, por meio do uso de agentes físicos e químicos, é denominado:",
     choices: [
-      "a partir de 14 anos",
-      "a partir de 16 anos",
-      "a partir de 18 anos",
-      "quando o profissional for qualificado e estiver em perfeitas condições de saúde",
+      "Esterilização.",
+      "Desinfecção.",
+      "Descontaminação.",
+      "Degermação.",
     ],
-    correct: 2,
-  },
-  {
-    question:
-      "NR7 - Ao assinalar 'inapto' em um Atestado de Saúde Ocupacional (ASO), o médico está afirmando:",
-    choices: [
-      "que o empregado não pode ser demitido",
-      "que o empregado não pode/poderia trabalhar em determinada função",
-      "que a pessoa não pode ser contratada",
-      "todas as alternativas anteriores estão corretas",
-    ],
-    correct: 1,
+    correct: 0,
   },
 ];
