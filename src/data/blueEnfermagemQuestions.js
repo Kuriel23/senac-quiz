04de10b0d880df41ac265147d5eeb4bdbab17c65
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-03-21 00:49:43 -0300

    Update blueEnfermagemQuestions.js

--- src/data/blueEnfermagemQuestions.js
+++ src/data/blueEnfermagemQuestions.js
@@ -1,47 +1,137 @@
 export const Questions = [
   {
     question:
-      "Assinale a alternativa com a situação em que o FGTS não poderá ser sacado::",
+      "O que é Fisiologia Humana?",
     choices: [
-      "Aposentadoria por invalidez.",
-      "Suspensão do trabalho avulso por período igual ou superior a 60 (sessenta) dias.",
-      "Rescisão do contrato por culpa recíproca ou força maior.",
-      "Rescisão do contrato por falecimento do empregador individual.",
+      "É um Ramo da Biologia que estuda o homem em todos as suas dimensões ",
+      "É um ramo da Biologia que estuda animais e plantas.",
+      "É um Ramo da Biologia que estuda animais.",
+      "É um Ramo da Biologia que estuda plantas.",
     ],
-    correct: 1,
-  },
-  {
-    question:
-      "O FGTS é um valor pago pela empresa e deve ser descontado do trabalhador.",
-    choices: ["Certo", "Errado"],
-    correct: 1,
-  },
-  {
-    question:
-      "Em caso de falecimento do trabalhador, sua conta do FGTS poderá ser movimentada por dependente habilitados ou alvará judicial indicando os sucessores do trabalhador falecido.",
-    choices: ["Certo", "Errado"],
     correct: 0,
   },
   {
-    question: "Qual o significado da sigla FGTS?",
+    question:
+      "Quais são os tipos de circulação sanguinea?",
+    choices: ["Circulação digestiva e respiratória.", "Circulação pulmonar e respiratória.", "Circulação pulmonar e a circulação sistêmica.", "Circulação sistêmica e digestiva."],
+    correct: 2,
+  },
+  {
+    question:
+      "O que é neurônio?",
+    choices: ["São as mais numerosas células sanguíneas.", "São as células que caracterizam o sistema nervoso, responsáveis por transmitir impulsos ao cérebro.", "Caracteriza-se por ser mais longo que os dendritos.", "É um tecido líquido que ocupa o interior dos ossos."],
+    correct: 1,
+  },
+  {
+    question: "Qual a função do Cerebelo?",
     choices: [
-      "Fundação Garantidora do Total de Serviços",
-      "Fundo de Garantia por Tempo de Seguridade",
-      "Fundação Garantidora por Tempo de Serviço",
-      "Fundo de Garantia por Tempo de Serviço",
+      "Funcionar como uma central de comando do corpo, sendo a parte mais importante do sistema nervoso.",
+      "É responsável por ações voluntárias e da musculatura esquelética do corpo.",
+      "Estimular o funcionamento normal dos órgãos e produz hormônios como a noradrenalina.",
+      "Coordenar todo o equilíbrio do corpo. ",
     ],
     correct: 3,
   },
   {
     question:
-      "Têm direito ao FGTS os trabalhadores individuais ou autônomos, ou seja, pessoas que não possuem vínculo empregatício.",
-    choices: ["Certo", "Errado"],
+      "O que é uma célula eucarionte?",
+    choices: ["Núcleo com membrana nuclear e nucléolo: DNA, RNA e Histonas: proteção do núcleo ao movimento citoesqueleto.", "São seres microscópicos constituídos de DNA ou RNA e protegidos por uma capa formada de proteínas.", "São organelas presentes em células eucarióticas e que se destacam por serem o local onde ocorre a respiração celular.", "Transmissão de informações entre neurônios se chama sinapse."],
     correct: 1,
   },
   {
     question:
-      "O FGTS tem como objetivo auxiliar o trabalhador financeiramente, caso este seja demitido, observadas determinadas regras.",
-    choices: ["Certo", "Errado"],
+      "O que é célula procarionte?",
+    choices: ["A proteção do corpo e o ponto de apoio para os músculos.", "Células que vivem em todos os ambientes concebíveis em todo o mundo", "Parte importante da defesa do corpo contra organismos infecciosos e substâncias estranhas", "Ausência de organelas com membranas e citoesqueleto "],
+    correct: 3,
+  },
+    {
+    question: "Qual o conceito de anatomia?",
+    choices: [
+      "É um órgão de forma piramidal, semelhante a uma bexiga",
+      "É um objeto de estudo os organismos vegetais, sendo portanto um ramo de estudo dos botânicos",
+      "Fundamental para que nutrientes e oxigênio cheguem a todas as células ",
+      "É a ciência que estuda, macro e microscopicamente, a constituição e o desenvolvimento do corpo humano ",
+    ],
+    correct: 3,
+  },
+      {
+    question: "Qual a função da epiglote?",
+    choices: [
+      "É responsável pela produção de enzimas, que ajudam na digestão dos alimentos, e de insulina, regulando os níveis de açúcar no sangue.",
+      "Impedir a ligação da faringe com a laringe, evitando, assim, a comunicação entre os aparelhos respiratório e digestório. ",
+      "Filtrar o sangue.",
+      "Servir de local para o desenvolvimento do bebê.",
+    ],
+    correct: 1,
+  },
+        {
+    question: "Em qual sistema é localizado o Pancreas?",
+    choices: [
+      "Sistema respiratório",
+      "Sistema nervoso ",
+      "Sistema digestório",
+      "Sistema reprodutor",
+    ],
+    correct: 2,
+  },
+          {
+    question: "Qual é o maior órgão do corpo humano?",
+    choices: [
+      "Pele",
+      "Instentino grosso ",
+      "Cérebro",
+      "Coração",
+    ],
     correct: 0,
+  },
+            {
+    question: "As trompas de falópio são presentes em qual sistema reprodutor?",
+    choices: [
+      "Digestório",
+      "Masculino",
+      "Nervoso",
+      "Feminino",
+    ],
+    correct: 3,
+  },
+              {
+    question: "O sêmen no corpo masculino é produzido em qual órgão?",
+    choices: [
+      "Testículos",
+      "Próstata",
+      "Pênis",
+      "Saco escrotal",
+    ],
+    correct: 0,
+  },
+                {
+    question: "Qual a função da próstata?",
+    choices: [
+      "É responsável pela nutrição e proteção dos gametas masculinos",
+      "Fabricar e servir como depósito de bactérias que auxiliam na digestão",
+      "Ajuda no processo de produção de proteínas dentro das células",
+      "Produzir o fluído que protege e nutre os espermatozoides no sêmen, tornando-o mais líquido ",
+    ],
+    correct: 3,
+  },
+                  {
+    question: "O líquido amniótico é o fluido que envolve o bebê em desenvolvimento e preenche a bolsa e a cavidade uterina.Qual a sua função?",
+    choices: [
+      "Saciar sede do bebê.",
+      "Além de manter a temperatura, o líquido proporciona proteção ao bebê contra traumas mecânicos, evita compressão do cordão umbilical e é fundamental para o desenvolvimento do feto.",
+      "Realizar a higiene do bebê ainda dentro do útero.",
+      "Fornecer nutrientes ao bebê. ",
+    ],
+    correct: 1,
+  },
+                    {
+    question: "Qual o procedimento necessário quando acontece de os rins não responderem?",
+    choices: [
+      "Quimioterapia",
+      "Fisioterapia",
+      "Hemodiálíse",
+      "Radioterapia",
+    ],
+    correct: 2,
   },
 ];
