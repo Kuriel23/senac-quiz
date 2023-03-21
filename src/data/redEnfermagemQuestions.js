90b4eafee8d3ca7b3722e380a9110dde122c660c
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-03-21 13:01:54 -0300

    Update redEnfermagemQuestions.js

--- src/data/redEnfermagemQuestions.js
+++ src/data/redEnfermagemQuestions.js
@@ -90,4 +90,154 @@ export const Questions = [
     choices: ["Veja", "A Patriota", "Forbes", "O Tico-Tico"],
     correct: 3,
   },
+    {
+    question: "De quem é a famosa frase 'Penso, logo existo'? ",
+    choices: [
+      "Platão",
+      "Deodoro da Fonseca",
+      "Descartes",
+      "Sócrates",
+    ],
+    correct: 2,
+  },
+      {
+    question: "Qual o nome do presidente do Brasil que ficou conhecido como Jango?",
+    choices: [
+      "Jacinto Anjos",
+      "Jânio Quadros",
+      "Getulho Vargas",
+      "João Goulart",
+    ],
+    correct: 3,
+  },
+        {
+    question: "O que é Filantropo?",
+    choices: [
+      "Pessoa com excesso de confiança",
+      "Pessoa egoísta",
+      "Pessoa que pratica a caridade",
+      "Pessoa fria",
+    ],
+    correct: 2,
+  },
+          {
+    question: "Qual o 1° presidente do Brasil?",
+    choices: [
+      "Deodoro da Fonseca",
+      "Luiz Inácio Lula da Silva",
+      "Floriano Peixoto",
+      "Floriano Peixoto",
+    ],
+    correct: 0,
+  },
+            {
+    question: "Como Adolf Hitler morreu ?",
+    choices: [
+      "Enforcado",
+      "Vítima de câncer",
+      "Decapitado",
+      "Suicídio",
+    ],
+    correct: 3,
+  },
+              {
+    question: "Em que país aconteceu o acidente do Césio-137?",
+    choices: [
+      "USA",
+      "Brasil",
+      "Rússia",
+      "Afeganistão",
+    ],
+    correct: 1,
+  },
+                {
+    question: "Em que governo brasileiro foi sancionada a lei das cotas para o ensino superior?",
+    choices: [
+      "Governo de Tancredo Neves",
+      "Governo de José Sarney",
+      "Governo de Dilma Rousseff",
+      "Governo de Luís Inácio Lula da Silva",
+    ],
+    correct: 2,
+  },
+                  {
+    question: "O que significa ONU?",
+    choices: [
+      "Organização das Nações Universitárias ",
+      " Organização das Nações Unidas",
+      "Orgão Nacional Estadunidense",
+      "Organismos Nacionais Únicos",
+    ],
+    correct: 1,
+  },
+                    {
+    question: "Qual o livro mais vendido no mundo a seguir à Bíblia?",
+    choices: [
+      "O Senhor dos Anéis",
+      "Dom Quixote",
+      "O Pequeno Príncipe",
+      "Ela, a Feiticeira",
+    ],
+    correct: 1,
+  },
+                      {
+    question: "Qual o livro mais vendido no mundo a seguir à Bíblia?",
+    choices: [
+      "O Senhor dos Anéis",
+      "Dom Quixote",
+      "O Pequeno Príncipe",
+      "Ela, a Feiticeira",
+    ],
+    correct: 1,
+  },
+                        {
+    question: "Angola é um país que se localiza no(a): ",
+    choices: [
+      "América do Norte",
+      "Arábia Saudita",
+      "Egito",
+      "África",
+    ],
+    correct: 3,
+  },
+                          {
+    question: "Mandarim é o idioma falado de qual país? ",
+    choices: [
+      "Coréia",
+      "País de Gales",
+      "China",
+      "Grécia",
+    ],
+    correct: 2,
+  },
+                            {
+    question: "Quem foi a primeira pessoa a pisar no espaço? ",
+    choices: [
+      "Yuri Gagarin",
+      "A cadela Laika",
+      "Neil Armstrong",
+      "Marcos Pontes",
+    ],
+    correct: 0,
+  },
+                              {
+    question: "Onde se localiza Machu Picchu?",
+    choices: [
+      "Colômbia",
+      "Peru",
+      "China",
+      "Bolívia",
+    ],
+    correct: 1,
+  },
+                                {
+    question: "Qual o maior planeta do sistema solar?",
+    choices: [
+      "Lua",
+      "Marte",
+      "Terra",
+      "Júpiter",
+    ],
+    correct: 3,
+  },
 ];
