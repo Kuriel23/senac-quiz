342014ede9e2a39635ecf26b74f6f0b8e00cb546
BrunoOSC <73199552+BrunoOSC@users.noreply.github.com>
2023-03-21 01:01:18 -0300

    Update blueQuestions.js

--- src/data/blueQuestions.js
+++ src/data/blueQuestions.js
@@ -75,4 +75,84 @@ export const Questions = [
     ],
     correct: 0,
   },
+        {
+    question:
+      "Posso continuar trabalhando após me aposentar?",
+    choices: [
+      "Verdadeiro", 
+      "Falso",
+    ],
+    correct: 0,
+  },
+          {
+    question:
+      "Quem são os contribuintes da Previdência Social?",
+    choices: [
+      "Empregado, trabalhador avulso, segurado especial, empregado doméstico, contribuinte individual e contribuinte facultativo. ", 
+      "Apenas desempregados.",
+      "Apenas idosos.",
+      "Apenas profissionais de ensino superior.",
+    ],
+    correct: 0,
+  },
+            {
+    question:
+      "O que é a prova de vida? ",
+    choices: [
+      "Um benefício oferecido pelo INSS", 
+      "Procedimento após um acidente de trabalho",
+      "Comprovação de que está vivo para continuar recebendo benefícios como aposentadoria ou pensão morte",
+      "Um benefício oferecido pelo FGTS",
+    ],
+    correct: 2,
+  },
+              {
+    question:
+      "Como saber se o benefício está liberado?",
+    choices: [
+      "Somente contratando um contador ou advogado", 
+      "Ver na empresa em que trabalha ou que trabalhou por último.",
+      "Ligando no número 135 ou acessando aplicativo Meu INSS ",
+      "Ligando na Caixa Econômica Federal",
+    ],
+    correct: 2,
+  },
+                {
+    question:
+      "Quem tem direito à aposentadoria por idade?",
+    choices: [
+      "Homens com 65 anos de idade e 20 anos de contribuição: Mulher com 62 anos e 15 anos de contribuição", 
+      "Homens com 62 anos de idade e 15 anos de contribuição: Mulher com 65 anos e 20 anos de contribuição",
+      "Homens com 60 anos de idade e 18 anos de contribuição: Mulher com 55 anos e 12 anos de contribuição",
+      "Homens com 58 anos de idade e 16 anos de contribuição: Mulher com 50 anos e 16 anos de contribuição",
+    ],
+    correct: 0,
+  },
+                  {
+    question:
+      "Diante da morte do segurado, o cônjuge ou companheiro e filhos menores de 18 anos de idade recebem a pensão por morte. ",
+    choices: [
+      "Verdadeiro", 
+      "Falso",
+    ],
+    correct: 1,
+  },
+                    {
+    question:
+      "INSS é a sigla de Instituto Nacional do Seguro Social. O INSS é o órgão responsável pelo pagamento de benefícios previdenciários como aposentadoria, seguro desemprego, auxílio-doença, pensão por morte e outros.",
+    choices: [
+      "Verdadeiro", 
+      "Falso",
+    ],
+    correct: 0,
+  },
+                      {
+    question:
+      "O recolhimento do INSS é feito pelas empresas e o descumprimento desta obrigatoriedade e do repasse dos valores para o órgão é crime. ",
+    choices: [
+      "Verdadeiro", 
+      "Falso",
+    ],
+    correct: 0,
+  },
 ];
